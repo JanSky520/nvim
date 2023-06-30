db988ee7375236460f4350e666c2ce4a24de3389
吾辈长歌 <79568068+JanSky520@users.noreply.github.com>
2023-06-30 13:53:48 +0800

    Update treesitter.lua

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -7,12 +7,5 @@ require'nvim-treesitter.configs'.setup {
         additional_vim_regex_highlighting = false,
     },
 
-    rainbow = {
-        enable = true,
-        --         -- Which query to use for finding delimiters
-        query = 'rainbow-parens',
-        -- Highlight the entire buffer all at once
-        strategy = require('ts-rainbow').strategy.global,
-    }
 
 }
